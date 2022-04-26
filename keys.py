d917f0588ccafcdf15891ee5fcc05771561abc88
Alessandra <ale.melo95@gmail.com>
2022-04-26 11:29:43 -0300

    first commit

---
+++ keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+misp_url = 'https://0.0.0.0/'
+misp_key = 'L601fQE3VhFQcettATkTgMVdQEjDHHsnRp7k82rg' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_verifycert = False
